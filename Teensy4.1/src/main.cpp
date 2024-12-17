28a49cef396f82497d9cee5fab0328f243fc8da3
astrisdesign <clayton@astrisdesign.com>
2024-12-16 22:44:57 -0800

    serial prints successful

--- Teensy4.1/src/main.cpp
+++ Teensy4.1/src/main.cpp
@@ -8,6 +8,8 @@
 
 void setup()
 {
+  Serial.begin(115200);       // Start serial communication at 115200 baud rate
+  delay(100); // Wait for serial port to connect
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -16,10 +18,12 @@ void loop()
 {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
+  // wait for a quarter second
   delay(250);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
-   // wait for a second
+   // wait for a quarter second
   delay(250);
+  // print serial debug message
+  Serial.println("TJ has great eyes!");
 }
