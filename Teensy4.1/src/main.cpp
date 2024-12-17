92f4538cb9beaf8f97e061b4109166a718f15490
astrisdesign <clayton@astrisdesign.com>
2024-12-16 23:04:14 -0800

    serial prints on multiple threads - stuttery?

--- Teensy4.1/src/main.cpp
+++ Teensy4.1/src/main.cpp
@@ -1,29 +1,44 @@
 /*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
+ * Multithreading test version 1
  */
 
 #include <Arduino.h>
+#include <TeensyThreads.h>
 
-void setup()
-{
-  Serial.begin(115200);       // Start serial communication at 115200 baud rate
-  delay(100); // Wait for serial port to connect
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+void ControlTask() {
+    while(true) {
+        Serial.println("ControlTask");
+        digitalWrite(LED_BUILTIN, HIGH); // blink on and off while waiting for a quarter second
+        threads.delay(125);
+        // turn the LED off by making the voltage LOW
+        digitalWrite(LED_BUILTIN, LOW);
+        // wait for a quarter second
+        threads.delay(125);
+    }
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a quarter second
-  delay(250);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-   // wait for a quarter second
-  delay(250);
-  // print serial debug message
-  Serial.println("TJ has great eyes!");
+void SensorTask() {
+    while(true) {
+        Serial.println("SensorTask");
+        threads.delay(500);
+    }
+}
+
+void CommsTask() {
+    while(true) {
+        Serial.println("CommsTask");
+        threads.delay(1000);
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    delay(10); // Wait for serial port to connect
+    threads.addThread(ControlTask);
+    threads.addThread(SensorTask);
+    threads.addThread(CommsTask);
+}
+
+void loop() {
+    threads.yield();
 }
