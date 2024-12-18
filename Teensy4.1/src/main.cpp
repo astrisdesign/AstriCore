de69b5ef884346b4d4b67e0649370ec4a1762675
astrisdesign <clayton@astrisdesign.com>
2024-12-17 22:34:13 -0800

    First motor spin test code

--- Teensy4.1/src/main.cpp
+++ Teensy4.1/src/main.cpp
@@ -1,20 +1,25 @@
 /*
- * Multithreading test version 1
+ * Motor testing - is this too pulse rate too slow?
  */
 
 #include <Arduino.h>
 #include <TeensyThreads.h>
 #include <Teensy41_Pinout.h>
 
+// Motor 1 pin definition
+const int pulse1 = pin33;
+const int dir1 = pin34;
+const int enable1 = pin35;
+
 void ControlTask() {
     while(true) {
         Serial.println("ControlTask");
-        digitalWrite(LED_BUILTIN, HIGH); // blink on and off while waiting for a quarter second-ish
-        threads.delay(127);
-        // turn the LED off by making the voltage LOW
-        digitalWrite(LED_BUILTIN, LOW);
-        // wait for a quarter second
-        threads.delay(127);
+        digitalWrite(LED_BUILTIN, HIGH); // blink on before pulse
+        digitalWrite(pulse1, HIGH);
+        threads.delay(1);
+        digitalWrite(pulse1, LOW);
+        digitalWrite(LED_BUILTIN, LOW); // blink off after pulse
+        threads.delay(1);
     }
 }
 
@@ -34,8 +39,19 @@ void CommsTask() {
 
 void setup() {
     Serial.begin(115200);
+
+    // Pin definitions
     pinMode(LED_BUILTIN, OUTPUT);  // LED pin is output
-    delay(10); // Wait for serial port to connect
+    pinMode(pulse1, OUTPUT);
+    pinMode(dir1, OUTPUT);
+    pinMode(enable1, OUTPUT);
+
+    digitalWrite(pulse1, LOW); // Start pulse low
+    digitalWrite(dir1, LOW); // Start with known dir
+    digitalWrite(enable1, LOW); // enable DM860T 
+
+    delay(200); // Wait for DM860T to enable
+
     threads.addThread(ControlTask);
     threads.addThread(SensorTask);
     threads.addThread(CommsTask);
