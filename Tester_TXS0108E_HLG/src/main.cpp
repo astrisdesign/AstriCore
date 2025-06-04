72f2f03968b692fc17479bfb05a4c6fe7f20210a
astrisdesign <clayton@astrisdesign.com>
2025-06-03 19:17:28 -0700

    Light all LEDs steady

--- Tester_TXS0108E_HLG/src/main.cpp
+++ Tester_TXS0108E_HLG/src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Assign LED pins based on netlist (Net-(U1-D1-A), Net-(U1-D2-A), etc.)
+constexpr int LED1_PIN = 23; // Net-(U1-D1-A)
+constexpr int LED2_PIN = 22; // Net-(U1-D2-A)
+constexpr int LED3_PIN = 1;  // Net-(U1-D3-A)
+constexpr int LED4_PIN = 3;  // Net-(U1-D4-A)
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Set LED pins as outputs
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  pinMode(LED3_PIN, OUTPUT);
+  pinMode(LED4_PIN, OUTPUT);
+
+  // Turn all LEDs on
+  digitalWrite(LED1_PIN, HIGH);
+  digitalWrite(LED2_PIN, HIGH);
+  digitalWrite(LED3_PIN, HIGH);
+  digitalWrite(LED4_PIN, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // TBD
 }
